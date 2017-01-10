3cbec3c7a626270a46ba740681fe2c2d0183a07d
okumura <okumura@tagbangers.co.jp>
2017-01-10 21:51:20 +0900

    scaffolding

--- Iruca/ViewController.swift
+++ Iruca/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,23 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	
+	@IBAction func presenceClick(_ sender: UIButton) {
+		
+	}
+	
+	@IBAction func awayClick(_ sender: UIButton) {
+	}
+	
+	@IBAction func quittingClick(_ sender: UIButton) {
+	}
 
-
+	@IBAction func meetingClick(_ sender: UIButton) {
+	}
+	
+	func apiCall(status:String){
+		
+	}
 }
 
